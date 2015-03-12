9b0ee64659ac50a7af3c9173a869db4498ac6895
Richard Baptist <rpbaptist@gmail.com>
2015-03-12 10:04:57 +0100

    Will move to markdown. Rework previews.

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -1,5 +1,5 @@
 
 def provide_summary(article)
   parsed_article = Nokogiri::HTML(article.summary)
-  parsed_article.search('section').inner_html
+  result = parsed_article.search('section').inner_html
 end
