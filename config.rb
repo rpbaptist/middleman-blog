c21db2676da4766211cf335888e46c4cccdb0cf7
Richard Baptist <rpbaptist@gmail.com>
2015-11-18 17:41:18 +0100

    Remove commented out code.

--- config.rb
+++ config.rb
@@ -37,36 +37,6 @@ end
 page "/feed.xml", layout: false
 
 ###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", layout: false
-#
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-###
 # Helpers
 ###
 
