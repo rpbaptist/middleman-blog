107841d9f63d06859482f5b6669643cbc365c1ce
Richard Baptist <rpbaptist@gmail.com>
2015-12-12 16:04:54 +0100

    Convert calendar erb to slim.

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -14,3 +14,14 @@ end
 def contact_button(contact_url, name)
   link_to '', contact_url, class: "fa fa-#{name}-square fa-3x footer--contact--item__link"
 end
+
+def calendar_title_date
+  case page_type
+  when 'day'
+    Date.new(year, month, day).strftime('%b %e %Y')
+  when 'month'
+    Date.new(year, month, 1).strftime('%b %Y')
+  when 'year'
+    year
+  end
+end
