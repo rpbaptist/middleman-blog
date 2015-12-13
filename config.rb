6c5b8ac3413f2b2ecac2bd9bd4a6328b92ef41ca
Richard Baptist <rpbaptist@gmail.com>
2015-12-13 23:01:23 +0100

    Move partials directory out of layouts directory. Create separate layout for blog posts.

--- config.rb
+++ config.rb
@@ -12,7 +12,7 @@ activate :blog do |blog|
   blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
   blog.taglink = "tags/{tag}.html"
-  # blog.layout = "layout"
+  blog.layout = "article_layout"
   blog.summary_separator = /(READMORE)/
   # blog.summary_length = 100000
   blog.year_link = "{year}.html"
