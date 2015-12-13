0b6f5f3a35a35132d4ef7dde633a7c5f0b1940a0
Richard Baptist <rpbaptist@gmail.com>
2015-12-13 23:57:20 +0100

    Remove sections and use built in summary method.

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -1,8 +1,3 @@
-def provide_summary(article)
-  parsed_article = Nokogiri::HTML(article.summary)
-  parsed_article.search('section').inner_html
-end
-
 def articles_by_category
   category_articles = { 'technical' => [], 'cultural' => [] }
   blog.articles.each do |article|
